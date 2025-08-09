c20ed0c58eb1e34a13dfa90dd2d839a42f53f551
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-08-08 19:14:40 -0500

    feat: finalizacion del proyecto

--- server/conexion_db.js
+++ server/conexion_db.js
@@ -1,11 +1,14 @@
+import dotenv from "dotenv";
 import mysql from "mysql2/promise"
 
+dotenv.config();  // Cargar variables de .env
+
 export const pool = mysql.createPool({
-    host: "localhost",
-    database: "biblioteca_easy",
-    port: "3306",
-    user: "root",
-    password: "Combita1234+",
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     connectionLimit: 10,        // Máximo número de conexiones activas al mismo tiempo
     waitForConnections: true,   // Si se alcanza el límite, las nuevas peticiones esperan su turno
     queueLimit: 0               // Número máximo de peticiones en espera (0 = sin límite)
